6db0a62737f224e98f08a61b82f5be819e2ad971
astrolince <astro@astrolince.com>
2020-06-12 23:58:53 -0300

    change violet

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@ var width = q.width = s.width;
 var height = q.height = s.height;
 var letters = Array(256).join(1).split('');
 
-var colors = ["#fc93bf", "#73fb98", "#aa0fe2"];
+var colors = ["#fc93bf", "#73fb98", "#9c56c3"];
 var color = colors[Math.floor(Math.random() * colors.length)];
 var draw = function () {
 
