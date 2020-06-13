5b33994d5f704633bb5a8a3d57866b666dd2e46a
frxnk <frxnco@protonamaill.com>
2020-06-12 23:23:22 -0300

    matrix new version 0.2

--- js/javascript.js
+++ js/javascript.js
@@ -3,10 +3,13 @@ var width = q.width = s.width;
 var height = q.height = s.height;
 var letters = Array(256).join(1).split('');
 
+var colors = ["#ff00ff", "#00ff72", "#aa0fe2"];
+var color = colors[Math.floor(Math.random() * colors.length)];
 var draw = function () {
+
     q.getContext('2d').fillStyle = 'rgba(0,0,0,.05)';
     q.getContext('2d').fillRect(0, 0, width, height);
-    q.getContext('2d').fillStyle = '#0F0';
+    q.getContext('2d').fillStyle = colors[Math.floor(Math.random() * colors.length)];
     letters.map(function (y_pos, index) {
         text = String.fromCharCode(3e4 + Math.random() * 33);
         x_pos = index * 30;
