e7ceb11a9877f429d222fc103d18918eba56031c
astrolince <astro@astrolince.com>
2020-08-07 18:41:56 -0300

    automatically resize canvas

--- js/matrix.js
+++ js/matrix.js
@@ -1,14 +1,15 @@
 window.addEventListener('load', function () {
     const canvas = document.getElementById('matrix')
-    const width = canvas.width = document.body.clientWidth * 0.99;
-    const height = canvas.height = document.body.clientHeight;
+    const context = canvas.getContext('2d');
     const letters = Array(256).join(1).split('');
     const colors = ['#fc93bf', '#73fb98', '#9c56c3'];
-    const context = canvas.getContext('2d');
+
+    canvas.width = document.body.clientWidth * 0.99;
+    canvas.height = document.body.clientHeight;
 
     const draw = () => {
         context.fillStyle = 'rgba(0,0,0,.05)';
-        context.fillRect(0, 0, width, height);
+        context.fillRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
         letters.map(function (y_pos, index) {
             text = String.fromCharCode(3e4 + Math.random() * 33);
@@ -19,4 +20,11 @@ window.addEventListener('load', function () {
     };
 
     setInterval(draw, 50);
+
+    const resizeCanvas = () => {
+        canvas.width = document.body.clientWidth * 0.99;
+        canvas.height = document.body.clientHeight;
+    }
+
+    window.onresize = resizeCanvas;
 });
