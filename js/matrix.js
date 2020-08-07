27246a9e3e9f653bf4e07168f8cc0351b69c73b1
astrolince <astro@astrolince.com>
2020-08-07 18:20:24 -0300

    fixing some ugly stuff with the matrix

---
+++ js/matrix.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('matrix')
+    const width = canvas.width = document.body.clientWidth * 0.99;
+    const height = canvas.height = document.body.clientHeight;
+    const letters = Array(256).join(1).split('');
+    const colors = ['#fc93bf', '#73fb98', '#9c56c3'];
+    const context = canvas.getContext('2d');
+
+    const draw = () => {
+        context.fillStyle = 'rgba(0,0,0,.05)';
+        context.fillRect(0, 0, width, height);
+        context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+        letters.map(function (y_pos, index) {
+            text = String.fromCharCode(3e4 + Math.random() * 33);
+            x_pos = index * 30;
+            context.fillText(text, x_pos, y_pos);
+            letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+        });
+    };
+
+    setInterval(draw, 50);
+});
