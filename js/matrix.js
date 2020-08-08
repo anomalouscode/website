a90591d076c41fef543011f1e0c6b902722e821d
astrolince <astro@astrolince.com>
2020-08-08 09:35:21 -0300

    improving symmetry

--- js/matrix.js
+++ js/matrix.js
@@ -4,8 +4,8 @@ window.addEventListener('load', function () {
     const letters = Array(256).join(1).split('');
     const colors = ['#fc93bf', '#73fb98', '#9c56c3'];
 
-    canvas.width = document.body.clientWidth * 0.99;
-    canvas.height = document.body.clientHeight;
+    canvas.width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) * 0.995;
+    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
     const draw = () => {
         context.fillStyle = 'rgba(0,0,0,.05)';
@@ -13,7 +13,7 @@ window.addEventListener('load', function () {
         context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
         letters.map(function (y_pos, index) {
             text = String.fromCharCode(3e4 + Math.random() * 33);
-            x_pos = index * 30;
+            x_pos = index * 25;
             context.fillText(text, x_pos, y_pos);
             letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
         });
@@ -22,8 +22,8 @@ window.addEventListener('load', function () {
     setInterval(draw, 50);
 
     const resizeCanvas = () => {
-        canvas.width = document.body.clientWidth * 0.99;
-        canvas.height = document.body.clientHeight;
+        canvas.width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) * 0.995;
+        canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     }
 
     window.onresize = resizeCanvas;
