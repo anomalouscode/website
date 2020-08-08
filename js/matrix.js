6cf6ad97415bc7c75f48e54213073797ae05c636
astrolince <astro@astrolince.com>
2020-08-08 10:30:31 -0300

    use arrow functions

--- js/matrix.js
+++ js/matrix.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', function () {
-    const canvas = document.getElementById('matrix')
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('matrix');
     const context = canvas.getContext('2d');
     const letters = Array(256).join(1).split('');
     const colors = ['#fc93bf', '#73fb98', '#9c56c3'];
@@ -11,11 +11,11 @@ window.addEventListener('load', function () {
         context.fillStyle = 'rgba(0,0,0,.05)';
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
-        letters.map(function (y_pos, index) {
+        letters.map((y_pos, index) => {
             text = String.fromCharCode(3e4 + Math.random() * 33);
             x_pos = index * 25;
             context.fillText(text, x_pos, y_pos);
-            letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+            letters[index] = y_pos > 758 + Math.random() * 1e4 ? 0 : y_pos + 10;
         });
     };
 
